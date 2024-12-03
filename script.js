7e00fdf0bcf90f610974e3c1899182ad999d777e
Alexand Shaymardanov <sovereing92@mail.ru>
2024-12-03 23:52:33 +0600

    Выполнение третьего задания!

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let title = "MyRepositoryForLearning";
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = Math.random() * 100000;
@@ -22,3 +24,35 @@ console.log(
   "Процент отката посреднику за работу " +
     (fullPrice * (rollback / 100)).toFixed(2)
 );
+
+title = prompt("Как называется наш проект");
+screens = prompt("Какие типы экранов нужно разработать");
+screenPrice = +prompt("Сколько будет стоить данная работа");
+adaptive = confirm("Нужен ли адаптив на сайте");
+let service1 = prompt("Какой тип услуги нужен");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой тип услуги нужен");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+
+console.log(title + " " + screens + " " + screenPrice + " " + adaptive);
+console.log(
+  service1 + ", " + servicePrice1 + ", " + service2 + ", " + servicePrice2
+);
+console.log("Фул присе" + fullPrice);
+console.log(servicePercentPrice);
+
+switch (true) {
+  case fullPrice > 30000:
+    console.log("Даем скидку в 10%");
+    break;
+  case fullPrice > 15000 && fullPrice <= 30000:
+    console.log("Даем скидку в 5%");
+    break;
+  case fullPrice > 0 && fullPrice <= 15000:
+    console.log("Скидка не предусмотрена");
+    break;
+  default:
+    console.log("Что то пошло не так");
+}
